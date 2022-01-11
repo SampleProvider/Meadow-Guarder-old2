cde682e7b2f058a251a7ba0f64debb3fd66fffae
maitian352 <68207888+maitian352@users.noreply.github.com>
2022-01-11 18:36:20 -0500

    Update bossbar.js

--- client/bossbar.js
+++ client/bossbar.js
@@ -2,8 +2,8 @@ startBossbar = function(name,hp,hpMax){
     if(bossbarDiv.style.display !== 'inline-block'){
         bossbarDiv.style.display = 'inline-block';
     }
-    bossbarValue.style.width = hp / hpMax * 500 + 'px';
-    bossbarText.innerHTML = name + ': ' + Math.round(hp) + ' / ' + hpMax;
+    bossbarValue.style.width = Math.max(hp,0) / hpMax * 500 + 'px';
+    bossbarText.innerHTML = name + ': ' + Math.max(Math.round(hp),0) + ' / ' + hpMax;
 }
 stopBossbar = function(){
     bossbarDiv.style.display = 'none';
