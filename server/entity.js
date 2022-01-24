959a7680a272843786e1c0675abae1cfd73a40ce
maitian352 <68207888+maitian352@users.noreply.github.com>
2022-01-24 18:39:32 -0500

    Fix Bugs

--- server/entity.js
+++ server/entity.js
@@ -3755,6 +3755,18 @@ Projectile = function(param){
                 if(Player.list[i].team !== self.team && Player.list[i].map === self.map && Player.list[i].hp > 0){
                     if(nearestEntity === null){
                         nearestEntity = Player.list[i];
+                        continue;
+                    }
+                    var direction = Math.atan2(Player.list[i].y - self.y,Player.list[i].x - self.x) / Math.PI * 180;
+                    direction = direction % 360;
+                    while(direction < 0){
+                        direction += 360;
+                    }
+                    if(direction - self.direction > 180){
+                        direction -= 180;
+                    }
+                    else if(direction - self.direction < -180){
+                        direction += 180;
                     }
                     else if(self.getDistance(Player.list[i]) < self.getDistance(nearestEntity)){
                         nearestEntity = Player.list[i];
